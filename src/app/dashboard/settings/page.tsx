c76b456325b8f924005f69603dc78f6a86b8dfec
Alejandro Giles <agilesrivas@MBPdeAlejandro.fibertel.com.ar>
2025-01-19 22:20:35 -0300

    Configuro opciones de sidebar

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function SettingsPage() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Settings</h1>
+        <p>Here you can manage users.</p>
+      </div>
+    );
+  }
+  
